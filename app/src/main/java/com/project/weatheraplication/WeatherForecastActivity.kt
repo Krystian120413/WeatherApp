ea01202467bf4b5f005eec51e97ccd3fd92ed899
Krystian120413 <Krystian12053@gmail.com>
2022-01-12 16:54:53 +0100

    Weather forecast with description

--- app/src/main/java/com/project/weatheraplication/WeatherForecastActivity.kt
+++ app/src/main/java/com/project/weatheraplication/WeatherForecastActivity.kt
@@ -14,6 +14,8 @@ import org.json.JSONObject
 import org.w3c.dom.Text
 import java.lang.reflect.Array
 import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
 
 class WeatherForecastActivity : AppCompatActivity() {
 
@@ -61,6 +63,16 @@ class WeatherForecastActivity : AppCompatActivity() {
                 val temp3 = main3.getJSONObject("temp")
                 val temp4 = main4.getJSONObject("temp")
 
+                val date1 = SimpleDateFormat("EEE dd/MM").format(Date(main1.getLong("dt")*1000))
+                val date2 = SimpleDateFormat("EEE dd/MM").format(Date(main2.getLong("dt")*1000))
+                val date3 = SimpleDateFormat("EEE dd/MM").format(Date(main3.getLong("dt")*1000))
+                val date4 = SimpleDateFormat("EEE dd/MM").format(Date(main4.getLong("dt")*1000))
+
+                val desc1 = main1.getJSONArray("weather").getJSONObject(0).getString("description")
+                val desc2 = main2.getJSONArray("weather").getJSONObject(0).getString("description")
+                val desc3 = main3.getJSONArray("weather").getJSONObject(0).getString("description")
+                val desc4 = main4.getJSONArray("weather").getJSONObject(0).getString("description")
+
                 val tempday1 = temp1.getInt("day").toString() + "°C"
                 val wind1 = (main1.getDouble("wind_speed")*3.6).toInt().toString() + " km/h"
                 val humidity1 = main1.getString("humidity") + "%"
@@ -75,20 +87,24 @@ class WeatherForecastActivity : AppCompatActivity() {
                 val humidity4 = main4.getString("humidity") + "%"
 
 
-                findViewById<TextView>(R.id.text1).text = "data tutaj"
+                findViewById<TextView>(R.id.text1).text = date1
                 findViewById<TextView>(R.id.temp1).text = tempday1
+                findViewById<TextView>(R.id.desc1).text = desc1
                 findViewById<TextView>(R.id.wind1).text = wind1
                 findViewById<TextView>(R.id.humidity1).text = humidity1
-                findViewById<TextView>(R.id.text2).text = "data tutaj"
+                findViewById<TextView>(R.id.text2).text = date2
                 findViewById<TextView>(R.id.temp2).text = tempday2
+                findViewById<TextView>(R.id.desc2).text = desc2
                 findViewById<TextView>(R.id.wind2).text = wind2
                 findViewById<TextView>(R.id.humidity2).text = humidity2
-                findViewById<TextView>(R.id.text3).text = "data tutaj"
+                findViewById<TextView>(R.id.text3).text = date3
                 findViewById<TextView>(R.id.temp3).text = tempday3
+                findViewById<TextView>(R.id.desc3).text = desc3
                 findViewById<TextView>(R.id.wind3).text = wind3
                 findViewById<TextView>(R.id.humidity3).text = humidity3
-                findViewById<TextView>(R.id.text4).text = "data tutaj"
+                findViewById<TextView>(R.id.text4).text = date4
                 findViewById<TextView>(R.id.temp4).text = tempday4
+                findViewById<TextView>(R.id.desc4).text = desc4
                 findViewById<TextView>(R.id.wind4).text = wind4
                 findViewById<TextView>(R.id.humidity4).text = humidity4
 
